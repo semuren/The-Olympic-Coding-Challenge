2c182d26977df8cf6ee5c2e88157d6f42cc1ff36
Svein-Erik <svemuren@gmail.com>
2019-07-27 00:36:12 +0200

    Moved files to folder

--- Exercise1.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-currentYear = datetime.now().year
-# Get the name from the user
-name = input("What is your name? ")
-# Get the age from the user
-age = int(input("How old are you? "))
-
-# Print out name, and calculate the year the user will turn 100
-print(name + ", you will turn 100 years old in year " + str(currentYear - age + 100))
